c6bc6a3e676bd925d1fea0d55b613c471ca5fecc
Sean <sean@vestaridefair.com>
2021-03-17 17:01:58 -0700

    Mar 16

--- components/traversy/Tasks.tsx
+++ components/traversy/Tasks.tsx
@@ -5,11 +5,9 @@ import Task from './Task';
 
 const Tasks = ({ tasks, onDelete, onToggle }) => (
   <>
-    <Flex alignItems="center">
-      
-    </Flex>
-    {tasks.map((task) => (
-      <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle} />
+    <Flex alignItems="center" />
+    {tasks.map((task, index) => (
+      <Task key={index} task={task} onDelete={onDelete} onToggle={onToggle} />
     ))}
   </>
 );
--- pages/youtube/tasktracker.tsx
+++ pages/youtube/tasktracker.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@chakra-ui/button';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import {
   Flex,
@@ -32,51 +32,109 @@ import AddTask from '../../components/traversy/AddTask';
 import FormButton from '../../components/traversy/FormButton';
 
 function toggle() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Doctors Appointment',
-      day: 'Sep 11, 2001',
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'Teachers Appointment',
-      day: 'Jan 11, 2012',
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: 'Coaches Appointment',
-      day: 'Sep 11, 2001',
-      reminder: false,
-    },
-  ]);
-  const deleteTask = (id) => {
+  const [tasks, setTasks] = useState([]);
+
+  // useEffect async to get it from server
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    };
+    getTasks();
+  }, []);
+
+  // originally a part of the useEffect above when first building this
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks');
+    const data = await res.json();
+
+    return data;
+  };
+
+  // delete tasks
+  // const deleteTask = (id) => {
+  //   setTasks(tasks.filter((task) => task.id !== id));
+  // };
+
+  // delete tasks with async now added to hook it up to server
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE',
+    });
     setTasks(tasks.filter((task) => task.id !== id));
   };
-  const reminderToggle = (id) => {
+
+  // adding new form items
+  // const addTask = (task) => {
+  //   const id = Math.floor(Math.random() * 10000) + 1;
+  //   const newTask = { id, ...task };
+  //   setTasks([...tasks, newTask]);
+  // };
+
+  // add tasks with async now added to hook it up to server
+  // in the video its called fetchTasks not addTask
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(task),
+    });
+    const data = await res.json();
+    setTasks([...tasks, data]);
+  };
+
+  // add task with async now added to hook it up to server
+  // in the video its called fetchTask
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+    return data;
+  };
+
+  // // toggle reminder
+  // const reminderToggle = (id) => {
+  //   setTasks(
+  //     tasks.map((task) =>
+  //       task.id === id ? { ...task, reminder: !task.reminder } : task
+  //     )
+  //   );
+  // };
+
+  // NOW SAVING IT TO SERVER toggle reminder
+  const reminderToggle = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+    const res = await fetch(`http://localhost/5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updTask),
+    });
+    const data = await res.json();
     setTasks(
       tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
+        task.id === id ? { ...task, reminder: data.reminder } : task
       )
     );
   };
 
-  // Show Form!
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1;
-    const newTask = { id, ...task };
-    setTasks([...tasks, newTask]);
-  };
+  // state toggles for showing and hiding the form
   const [showForm, setShowForm] = useState(false);
+  const showAdd = { showForm };
+
+  // The Actual Components:
+
   return (
     <>
       <VStack>
         <Heading fontWeight="500" mt={10} mb={4}>
           The Task Tracker App
         </Heading>{' '}
-        <FormButton onAdd={() => setShowForm(!showForm)} showAdd={showForm} />
+        <FormButton onAdd={() => setShowForm(!showForm)} />
         {tasks.length > 0 && (
           <Heading fontSize={28} fontWeight="300">
             {tasks.length} tasks!
